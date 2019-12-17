9f9154eae82066a1461dffb3680bce2f6004d97b
Comus Leong <comus.leong@gmail.com>
2019-12-18 00:24:16 +0800

    update

---
+++ bin/allmight.js
@@ -0,0 +1,50 @@
+const { spawn } = require('child_process')
+
+const yarnRun = async (scriptName) => {
+  return new Promise((resolve, reject) => {
+    const stream = spawn(
+      process.platform === 'win32' ? 'yarn.cmd' : 'yarn',
+      ['run', scriptName],
+      {
+        cwd: __dirname,
+        stdio: 'inherit',
+        env: {
+          ...process.env,
+          CWD: process.cwd()
+        }
+      }
+    )
+
+    stream.on('close', code => {
+      resolve()
+    })
+
+    stream.on('error', err => {
+      console.log(`yarnRun ${scriptName} error`, err)
+      reject(err)
+    })
+
+    stream.on('exit', code => {
+      console.log(`yarnRun ${scriptName} exit code`, code)
+      if (code !== 0) {
+        reject(new Error(`yarnRun ${scriptName} exit code error`))
+      }
+    })
+  })
+}
+
+const start = async () => {
+  await yarnRun('echo')
+}
+
+start()
+
+process.on('unhandledRejection', async err => {
+  console.error('Unhandled rejection', err)
+  process.exit(1)
+})
+
+process.on('uncaughtException', async err => {
+  console.error('Uncaught exception', err)
+  process.exit(1)
+})
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  Text
+} from 'react-native'
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <Text>this is allmight default screen!</Text>
+      </SafeAreaView>
+    </>
+  )
+}
+
+export default App
