b40893a9da472f534284c52998ff4c9a92d0e552
Comus Leong <comus.leong@gmail.com>
2019-12-18 15:31:53 +0800

    support code push android (hardcode staging for now)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   SafeAreaView,
-  Text
+  Text,
+  Alert
 } from 'react-native'
+import codePush from 'react-native-code-push'
 
-const App = () => {
-  return (
-    <>
-      <SafeAreaView>
-        <Text>this is allmight default screen!</Text>
-      </SafeAreaView>
-    </>
-  )
+class App extends Component {
+  state = {
+    progress: ''
+  }
+
+  codePushStatusDidChange (status) {
+    switch (status) {
+      case codePush.SyncStatus.CHECKING_FOR_UPDATE:
+        console.log('Checking for updates.')
+        Alert.alert('Checking for updates.')
+        break
+      case codePush.SyncStatus.DOWNLOADING_PACKAGE:
+        console.log('Downloading package.')
+        Alert.alert('Downloading package.')
+        break
+      case codePush.SyncStatus.INSTALLING_UPDATE:
+        console.log('Installing update.')
+        Alert.alert('Installing update.')
+        break
+      case codePush.SyncStatus.UP_TO_DATE:
+        console.log('Up-to-date.')
+        Alert.alert('Up-to-date.')
+        break
+      case codePush.SyncStatus.UPDATE_INSTALLED:
+        console.log('Update installed.')
+        Alert.alert('Update installed.')
+        break
+    }
+  }
+
+  codePushDownloadDidProgress (progress) {
+    this.setState({
+      progress: `${progress.receivedBytes} of  ${progress.totalBytes} received.`
+    })
+  }
+
+  render () {
+    return (
+      <>
+        <SafeAreaView>
+          <Text>Code Push</Text>
+          <Text>{this.state.progress}</Text>
+        </SafeAreaView>
+      </>
+    )
+  }
 }
 
-export default App
+export default codePush(App)
