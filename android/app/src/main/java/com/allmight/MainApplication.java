1974d83d2206f9035d77f51d832358e34c9ec2b8
BenPeng <asd5530025@gmail.com>
2019-12-18 15:37:11 +0800

    1. add ReactNativeBeaconManager

--- android/app/src/main/java/com/allmight/MainApplication.java
+++ android/app/src/main/java/com/allmight/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.mackentoch.beaconsandroid.BeaconsAndroidPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
