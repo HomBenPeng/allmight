f9997a7633e351af9210347aa1765f67d9e24ea4
Comus Leong <comus.leong@gmail.com>
2019-12-18 10:02:41 +0800

    update

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
@@ -6,5 +8,21 @@ module.exports = {
         inlineRequires: false
       }
     })
+  },
+  watchFolders: [
+    path.resolve(__dirname, '../../src')
+  ],
+  resolver: {
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => {
+          if (target.hasOwnProperty(name)) { // eslint-disable-line
+            return target[name]
+          }
+          return path.join(process.cwd(), `node_modules/${name}`)
+        }
+      }
+    )
   }
 }
