4837e14dd318068df2b63155b3f03f3141a13227
Comus Leong <comus.leong@gmail.com>
2019-12-19 13:34:56 +0800

    beacon manager ios just works

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Alert
 } from 'react-native'
 import codePush from 'react-native-code-push'
+import Beacons from 'react-native-beacons-manager'
 
 class App extends Component {
   state = {
@@ -40,6 +41,51 @@ class App extends Component {
     this.setState({
       progress: `${progress.receivedBytes} of  ${progress.totalBytes} received.`
     })
+  }
+
+  componentDidMount () {
+    const region = {
+      identifier: '',
+      uuid: 'b5b182c7-eab1-4988-aa99-b5c1517008d9'
+    }
+
+    Beacons.requestWhenInUseAuthorization()
+
+    Beacons.getAuthorizationStatus(status => {
+      if (status === 'denied') {
+        Alert.alert('不能偵測 Beacon', '手機沒有開放位置權限')
+      }
+    })
+
+    Beacons
+      .startRangingBeaconsInRegion(region)
+      .then(() => console.log('Beacons ranging started succesfully'))
+      .catch(error => console.log(`Beacons ranging not started, error: ${error}`))
+
+    this.beaconsDidRangeEvent = Beacons.BeaconsEventEmitter.addListener(
+      'beaconsDidRange',
+      ({ region: { identifier, uuid }, beacons }) => {
+        // do here anything you need (ex: setting state...)
+        console.log('beaconsDidRange these beacons: ', beacons)
+      }
+    )
+  }
+
+  componentWillUnMount () {
+    const region = {
+      identifier: 'Estimotes',
+      uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
+    }
+
+    Beacons
+      .stopRangingBeaconsInRegion(region)
+      .then(() => console.log('Beacons ranging stopped succesfully'))
+      .catch(error => console.log(`Beacons ranging not stopped, error: ${error}`))
+
+    // remove beacons event we registered at componentDidMount
+    if (this.beaconsDidRangeEvent) {
+      this.beaconsDidRangeEvent.remove()
+    }
   }
 
   render () {
