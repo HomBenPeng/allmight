ac8dfe1bc1775307d6e52c652b0bb1bc173a9bf2
Comus Leong <comus.leong@gmail.com>
2019-12-19 15:44:38 +0800

    update app.js to support android beacon manager code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import {
   SafeAreaView,
   Text,
-  Alert
+  Alert,
+  Platform,
+  PermissionsAndroid
 } from 'react-native'
 import codePush from 'react-native-code-push'
 import Beacons from 'react-native-beacons-manager'
@@ -49,26 +51,76 @@ class App extends Component {
       uuid: 'b5b182c7-eab1-4988-aa99-b5c1517008d9'
     }
 
-    Beacons.requestWhenInUseAuthorization()
+    if (Platform.OS === 'ios') {
+      Beacons.requestWhenInUseAuthorization()
 
-    Beacons.getAuthorizationStatus(status => {
-      if (status === 'denied') {
-        Alert.alert('不能偵測 Beacon', '手機沒有開放位置權限')
-      }
-    })
+      Beacons.getAuthorizationStatus(status => {
+        if (status === 'denied') {
+          Alert.alert('不能偵測 Beacon', '手機沒有開放位置權限')
+        }
+      })
+    }
+
+    if (Platform.OS === 'android') {
+      this.checkLocationPermission()
+      Beacons.detectIBeacons()
+    }
 
     Beacons
       .startRangingBeaconsInRegion(region)
       .then(() => console.log('Beacons ranging started succesfully'))
       .catch(error => console.log(`Beacons ranging not started, error: ${error}`))
 
-    this.beaconsDidRangeEvent = Beacons.BeaconsEventEmitter.addListener(
-      'beaconsDidRange',
-      ({ region: { identifier, uuid }, beacons }) => {
-        // do here anything you need (ex: setting state...)
-        console.log('beaconsDidRange these beacons: ', beacons)
+    if (Platform.OS === 'ios') {
+      this.beaconsDidRangeEvent = Beacons.BeaconsEventEmitter.addListener(
+        'beaconsDidRange',
+        ({ region: { identifier, uuid }, beacons }) => {
+          // do here anything you need (ex: setting state...)
+          console.log('beaconsDidRange these beacons: ', beacons)
+        }
+      )
+    }
+
+    if (Platform.OS === 'android') {
+      this.beaconsDidRangeEvent = Beacons.BeaconsEventEmitter.addListener(
+        'beaconsDidRange',
+        (data) => {
+          console.log('beaconsDidRange data: ', data)
+        }
+      )
+    }
+  }
+
+  checkLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED || granted === true) {
+      } else {
+        this.requestLocationPermission()
       }
-    )
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+
+  // 註冊 Location 權限
+  requestLocationPermission = async () => {
+    try {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        {
+          title: '申請權限',
+          message:
+            '申請權限',
+          buttonNegative: 'NO',
+          buttonPositive: 'OK'
+        }
+      )
+    } catch (err) {
+      console.warn(err)
+    }
   }
 
   componentWillUnMount () {
