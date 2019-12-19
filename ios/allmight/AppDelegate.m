d943da8ebdd02274bb237d1e1251ab63861fd0ed
Comus Leong <comus.leong@gmail.com>
2019-12-19 11:11:24 +0800

    add codepush for ios

--- ios/allmight/AppDelegate.m
+++ ios/allmight/AppDelegate.m
@@ -15,6 +15,8 @@
 #import <AppCenterReactNativeAnalytics.h>
 #import <AppCenterReactNativeCrashes.h>
 
+#import <CodePush/CodePush.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -43,7 +45,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
