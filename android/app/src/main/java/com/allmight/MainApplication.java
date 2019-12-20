cda42e0c609eff06740087be1af00a43f167d301
BenPeng <asd5530025@gmail.com>
2019-12-20 13:45:32 +0800

    1. add @react-native-firebase/messaging

--- android/app/src/main/java/com/allmight/MainApplication.java
+++ android/app/src/main/java/com/allmight/MainApplication.java
@@ -17,6 +17,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage;
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
