65c519791cfc0e21cec157a630234522ec14ac53
BenPeng <asd5530025@gmail.com>
2019-12-23 18:18:19 +0800

    1. add firebase message

--- android/app/src/main/java/dev/allmight/MainApplication.java
+++ android/app/src/main/java/dev/allmight/MainApplication.java
@@ -10,7 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -34,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           packages.add(new RNFirebaseAuthPackage());
+          packages.add(new RNFirebaseMessagingPackage());
           return packages;
         }
 
