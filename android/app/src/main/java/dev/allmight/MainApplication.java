19ebd34e6f5db379a7f33f38114dd12b38e1af73
BenPeng <asd5530025@gmail.com>
2019-12-24 18:37:30 +0800

    1. add react-native-vector-icons

--- android/app/src/main/java/dev/allmight/MainApplication.java
+++ android/app/src/main/java/dev/allmight/MainApplication.java
@@ -11,9 +11,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
-
+import com.oblador.vectoricons.VectorIconsPackage;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
