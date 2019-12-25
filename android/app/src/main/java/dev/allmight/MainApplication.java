53487495ca21dd39f714a6b9e6a5ebbb6ac80de1
BenPeng <asd5530025@gmail.com>
2019-12-25 16:51:41 +0800

    1. android add io.invertase.firebase.notifications.RNFirebaseNotificationsPackage

--- android/app/src/main/java/dev/allmight/MainApplication.java
+++ android/app/src/main/java/dev/allmight/MainApplication.java
@@ -14,6 +14,7 @@ import java.util.List;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
           // packages.add(new MyReactNativePackage());
           packages.add(new RNFirebaseAuthPackage());
           packages.add(new RNFirebaseMessagingPackage());
+          packages.add(new RNFirebaseNotificationsPackage());
           return packages;
         }
 
